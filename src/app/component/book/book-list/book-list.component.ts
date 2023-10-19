3034ea10302721f25036ab39e8252ee818b9598f
Никита <tsybin07@mail.ru>
2023-10-19 20:55:11 +0400

    temp

--- src/app/component/book/book-list/book-list.component.ts
+++ src/app/component/book/book-list/book-list.component.ts
@@ -3,6 +3,9 @@ import {CommonModule} from '@angular/common';
 import {BookService} from "../../../service/book.service";
 import {Book} from "../../../model/book";
 import {GenreEnum} from "../../../enumeration/genre-enum";
+import {Author} from "../../../model/author";
+import {AuthorService} from "../../../service/author.service";
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-book-list',
@@ -13,27 +16,43 @@ import {GenreEnum} from "../../../enumeration/genre-enum";
 })
 export class BookListComponent implements OnInit {
 
+  protected readonly GenreEnum = GenreEnum;
+
+  authors: Author[] = [];
+
   books: Book[] = [];
 
-  getBooks(): void {
-    this.bookService.getBooks()
-      .subscribe(books => {
-        this.books = books;
-      })
-
-  }
-
-  init(): void {
-    this.getBooks();
-  }
-
-  constructor(private bookService: BookService) {
+  constructor(private authorService: AuthorService, private bookService: BookService) {
   }
 
   ngOnInit(): void {
     this.init();
   }
 
+  init(): void {
+    this.getAuthors();
+    this.getBooks();
+  }
 
-  protected readonly GenreEnum = GenreEnum;
+  closeBookModal(): void {
+    /*const bookModal  = new bootstrap.Modal('#bookModal', {
+      keyboard: false
+    });
+    bookModal.hide();*/
+  }
+
+  private getBooks(): void {
+    this.bookService.getBooks()
+      .subscribe(books => {
+        this.books = books;
+      })
+  }
+
+  private getAuthors(): void {
+    this.authorService.getAuthors()
+      .subscribe(authors => {
+        this.authors = authors;
+      })
+  }
+
 }
--- src/app/service/author.service.ts
+++ src/app/service/author.service.ts
@@ -2,20 +2,20 @@ import {Injectable} from '@angular/core';
 import {ApiConstant} from "../constant/api-constant";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Book} from "../model/book";
+import {Author} from "../model/author";
 
 @Injectable({
   providedIn: 'root'
 })
-export class BookService {
+export class AuthorService {
 
-  private booksUrl: string = ApiConstant.BACKEND_API_URL + ApiConstant.URL_SEPARATOR + "books";
+  private authorsUrl: string = ApiConstant.BACKEND_API_URL + ApiConstant.URL_SEPARATOR + "authors";
 
   constructor(private http: HttpClient) {
   }
 
-  getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.booksUrl);
+  getAuthors(): Observable<Author[]> {
+    return this.http.get<Author[]>(this.authorsUrl);
   }
 
 }
